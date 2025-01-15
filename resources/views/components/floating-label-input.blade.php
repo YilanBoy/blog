0f202ec0a49d88e853a1a438847f939f41d4e3d7
Allen <allenjianggo@gmail.com>
2025-01-15 16:05:15 +0800

    refactor: change floating label input style

--- resources/views/components/floating-label-input.blade.php
+++ resources/views/components/floating-label-input.blade.php
@@ -1,15 +1,14 @@
 @props(['id', 'placeholder'])
 
-<div class="relative px-4 pb-4 pt-5">
+<div {{ $attributes->merge(['class' => 'relative']) }}>
   <input
-    class="peer h-12 w-full border-b-2 border-gray-300 bg-transparent text-gray-900 placeholder-transparent transition duration-150 ease-in focus:border-emerald-500 focus:outline-none dark:border-gray-400 dark:text-gray-50 dark:focus:border-lividus-500"
+    class="dark:focus:border-lividus-500 peer h-16 w-full rounded-lg border-2 border-gray-300 bg-transparent p-4 text-gray-900 placeholder-transparent transition duration-150 ease-in focus:border-emerald-500 focus:outline-none dark:border-gray-400 dark:text-gray-50"
     id="{{ $id }}"
     placeholder="{{ $placeholder }}"
-    {{ $attributes->merge() }}
   >
 
   <label
-    class="pointer-events-none absolute left-4 top-1 text-sm text-gray-600 transition-all peer-placeholder-shown:top-7 peer-placeholder-shown:text-lg peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-sm peer-focus:text-gray-600 dark:text-gray-50 selection:dark:text-gray-50 dark:peer-placeholder-shown:text-gray-400 dark:peer-focus:text-gray-50"
+    class="pointer-events-none absolute -top-2 left-4 bg-gray-50 px-2 text-base text-gray-600 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-lg peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:text-base peer-focus:text-gray-600 dark:bg-gray-800 dark:text-gray-50 selection:dark:text-gray-50 dark:peer-placeholder-shown:text-gray-400 dark:peer-focus:text-gray-50"
     for="{{ $id }}"
   >
     {{ $placeholder }}
